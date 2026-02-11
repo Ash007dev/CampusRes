619eb9b84c09f7a0336db471bf2f76e01aecdc59
Darun R G <cb.sc.u4cse23215@cb.students.amrita.edu>
2026-02-11 14:02:14 +0530

    Fixed test file

--- write.py
+++
@@ -1,8 +0,0 @@
-# Writing text into a file
-
-file = open("output.txt", "w")  # "w" mode creates file or overwrites if exists
-for i in range(45000):
-    file.write("Hello, this is my first text file.\n")
-file.close()
-
-print("Text written successfully!")
